83b94e85bd7a7e01c42672bd4a23905e7b27996b
Vitória Vicente <viviicente@outlook.com>
2022-07-21 22:34:01 -0300

    projeto spring basico

---
+++ meuprojetoSpring2/src/main/java/br/dio/com/primeiroprojSpring/meuprojetoSpring2/MeuprojetoSpring2Application.java
@@ -0,0 +1,13 @@
+package br.dio.com.primeiroprojSpring.meuprojetoSpring2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeuprojetoSpring2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeuprojetoSpring2Application.class, args);
+	}
+
+}
---
+++ meuprojetoSpring2/src/test/java/br/dio/com/primeiroprojSpring/meuprojetoSpring2/MeuprojetoSpring2ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.dio.com.primeiroprojSpring.meuprojetoSpring2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeuprojetoSpring2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
